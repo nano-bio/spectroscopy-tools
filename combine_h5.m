1e0b69babdf31642c178e86887d19a0ffdbb5731
Johannes Postler <johannes.postler@uibk.ac.at>
2015-11-15 13:02:01 +0100

    Use several writes, if buffers are not enough

--- combine_h5.m
+++ combine_h5.m
@@ -2,19 +2,25 @@
 % one, where the writes are the sum-spectra of the original files
 
 % new file to use
-newh5 = 'new.h5';
+newh5 = 'new3.h5';
 
 % output file
-outputfile = 'filelist_864nm.txt';
+outputfile = 'filelist_bereich_3_test2.txt';
 
 % directory to combine
-directory = '864nm';
+directory = 'bereich_3_2_fine_960nm';
 h5files = dir([directory '/*.h5']);
 
 newbuffer = 1;
 firstrun = 1;
+newwrite = 1;
 
 outputfilehandle = fopen(outputfile,'a'); 
+
+timebinsinnewh5 = getnumberofinstancesinh5(newh5, 'timebins');
+writesnew = getnumberofinstancesinh5(newh5, 'writes');
+buffersnew = getnumberofinstancesinh5(newh5, 'buffers');
+signalsum = zeros(timebinsinnewh5, 1);
 
 for filenumber = 1:length(h5files)
     fn = h5files(filenumber).name;
@@ -24,22 +30,40 @@ for filenumber = 1:length(h5files)
     writes = getnumberofinstancesinh5(fullfile(directory,fn), 'writes');
     buffers = getnumberofinstancesinh5(fullfile(directory,fn), 'buffers');
     fprintf(outputfilehandle, '%s\n', fn);
-    
+	
     for buffer = 1:buffers
-       for write = 1:writes
-           signal = h5read(fullfile(directory,fn),'/FullSpectra/TofData', [1 1 buffer write], [timebins 1 1 1]);
-           if firstrun == 1
-               signalsum = zeros(size(signal));
-               firstrun = 0;
-           end
-           signalsum = signalsum + signal;
-           h5write(newh5, '/FullSpectra/TofData', signal, [1 1 newbuffer 1], [timebins 1 1 1]);
-           newbuffer = newbuffer + 1;
-       end
+		for write = 1:writes
+            if timebins >= timebinsinnewh5
+                signal = h5read(fullfile(directory,fn),'/FullSpectra/TofData', [1 1 buffer write], [timebinsinnewh5 1 1 1]);
+            else
+                signal = h5read(fullfile(directory,fn),'/FullSpectra/TofData', [1 1 buffer write], [timebins 1 1 1]);
+                timebindifference = timebinsinnewh5 - timebins;
+                signal(end+1:end+timebindifference) = 0;
+            end
+            
+            newbuffer
+            newwrite
+            signalsum = signalsum + signal;
+			h5write(newh5, '/FullSpectra/TofData', signal, [1 1 newbuffer newwrite], [timebinsinnewh5 1 1 1]);
+			
+		    if newbuffer < buffersnew
+			    newbuffer = newbuffer + 1;
+            elseif newbuffer == buffersnew
+				newbuffer = 1;
+				newwrite = newwrite + 1;
+			end
+		end
     end
 end
-timebinsinnewh5 = getnumberofinstancesinh5(newh5, 'timebins');
-timebindifference = timebinsinnewh5 - timebins;
-signalsum(end+1:end+timebindifference) = 0;
+
+finalsize = size(h5read(newh5, '/FullSpectra/SumSpectrum'), 1);
+sumsize = size(signalsum, 1);
+if finalsize > sumsize
+    diff = finalsize - sumsize;
+    signalsum(end+1:end+diff) = 0;
+elseif finalsize < sumsize
+    signalsum = signalsum(1:finalsize);
+end
+
 h5write(newh5, '/FullSpectra/SumSpectrum', signalsum);
 fclose(outputfilehandle);
